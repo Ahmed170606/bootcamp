c9ff1dcadf9fdd79c2ba5f8ef1a9dcd233fa7628
Ahmed170606 <143695629+Ahmed170606@users.noreply.github.com>
2023-09-13 12:56:43 +0200

    comiit

---
+++ Deel_2/Les_12/uitwerkingen/les12_opdr2.py
@@ -0,0 +1,13 @@
+nummer1 = float (input('vul nummer1'))
+nummer2 = float (input('vul nummer2'))
+
+berekening = nummer1 * nummer2
+
+
+print(f'we hebben nu {nummer1} keer {nummer2}, nu gaan we gedeeld door')
+
+
+
+berekening2 = berekening / 2
+
+print(f'antwoordt {berekening2}')
---
+++ Deel_2/Les_12/uitwerkingen/les12_opdr3.py
@@ -0,0 +1,5 @@
+woord = 'aaaaaaaaaaaaaaaaaaaaaaaaaaa'
+aantal_tekens = len(woord)
+
+
+print(f"Het woord is: {woord} en bestaat uit {aantal_tekens} tekens.")

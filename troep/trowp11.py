bed8eddefb8a6546523b51ba4572f73159a98419
Ahmed170606 <143695629+Ahmed170606@users.noreply.github.com>
2023-09-14 11:34:10 +0200

    yuyu

---
+++ troep/trowp11.py
@@ -0,0 +1,15 @@
+poke = ['yur']
+
+poke.remove('yur')
+
+
+poke.insert(3, 'x1x1')
+
+for p in range(4):
+    next_poke = input('a poke?')
+    poke.append(next_poke)
+
+
+print(poke)
+for p in poke:
+    print(p)

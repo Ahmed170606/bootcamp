a879195d6baeb6ba2346c9975e325e6c2b216b52
Ahmed170606 <143695629+Ahmed170606@users.noreply.github.com>
2023-09-27 12:03:17 +0200

    c1

---
+++ tussentoensdeel2/opdr7.py
@@ -0,0 +1,3 @@
+# Print onder elkaar de getallen 1-250 met max 2 regels code.
+for list in range (1,250):
+    print(list)
---
+++ tussentoensdeel2/opdr9.py
@@ -0,0 +1,17 @@
+# Gegeven is:
+lijst_eten = ['appel', 'pannenkoek', 'kiwi', 'hamburger']
+# a: print een eenvoudig menu met de volgende layout:
+
+print("Onze menukaart:")
+for menukaart in lijst_eten:
+    print(menukaart)
+
+# Onze menukaart:
+# appel
+# pannenkoek
+# kiwi
+# hamburger 
+
+# b: Schrijf code die ervoor zorgt dat alleen het eten met de langste naam wordt geprint in de terminal.
+#  # Let op: je moet in de code eerst bepalen welk eten de langste naam heeft (en dus niet hardcoded 1 voor de index gebruiken).
+#  # test je code door extra eten toe te voegen met een nog langere naam.
